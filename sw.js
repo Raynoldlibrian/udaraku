d69fdd3634730972de33b3ac2d1e14240f58367b
Raynoldlibrian <97292115+Raynoldlibrian@users.noreply.github.com>
2026-09-07 17:03:45 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'udaraku-shell-v7';
+const CACHE = 'udaraku-shell-v9';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
