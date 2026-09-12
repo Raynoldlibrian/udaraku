204f257a9398a3689352b3a1363638e15b040eb2
Raynoldlibrian <97292115+Raynoldlibrian@users.noreply.github.com>
2026-09-13 05:15:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'udaraku-shell-v9';
-const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'udaraku-shell-v12';
+const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './peta-dasar-inhu.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
